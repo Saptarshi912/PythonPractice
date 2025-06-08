dcef4028ab7ca6c8860793484a6fb5ca5d36bd68
Saptarshi <growthpersonal874@gmail.com>
2025-06-08 21:52:04 +0530

    added date time and a singleton function

--- singleton/Singleton.py
+++ singleton/Singleton.py
@@ -15,10 +15,11 @@ class Singelton_Service_One(metaclass=SingletonMeta):
         self.value = value
         self.counter = 0
         self.start_time = datetime.now()
-    def get_count_info(self):
+
+    def get_count_info(self, current_time=datetime.now()):
         return f"Counter: {self.counter}, " \
                f"Start Time: {self.start_time.strftime('%Y-%m-%d %H:%M:%S')}," \
-               f"End Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+               f"End Time: {current_time.strftime('%Y-%m-%d %H:%M:%S')}"
 
 class Singelton_Service_Two(metaclass=SingletonMeta):
 
@@ -26,10 +27,11 @@ class Singelton_Service_Two(metaclass=SingletonMeta):
         self.value = value
         self.counter = 0
         self.start_time = datetime.now()
-    def get_count_info(self):
+
+    def get_count_info(self, current_time=datetime.now()):
         return f"Counter: {self.counter}, " \
                f"Start Time: {self.start_time.strftime('%Y-%m-%d %H:%M:%S')}," \
-               f"End Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+               f"End Time: {current_time.strftime('%Y-%m-%d %H:%M:%S')}"
 
 class Singelton_Service_Three(metaclass=SingletonMeta):
 
@@ -37,24 +39,26 @@ class Singelton_Service_Three(metaclass=SingletonMeta):
         self.value = value
         self.counter = 0
         self.start_time = datetime.now()
-    def get_count_info(self):
+    def get_count_info(self,current_time=datetime.now()):
         return f"Counter: {self.counter}, " \
                f"Start Time: {self.start_time.strftime('%Y-%m-%d %H:%M:%S')}," \
-               f"End Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+               f"End Time: {current_time.strftime('%Y-%m-%d %H:%M:%S')}"
 
 s11 = Singelton_Service_One("Singleton Service call One")
 s21 = Singelton_Service_Two("Singleton Service call Two")
 s31 = Singelton_Service_Three("Singleton Service call Three")
 for i in range(10):
     s11.counter += 1
+    print(s11.get_count_info(datetime.now()))
     if i % 2 == 0:
         time.sleep(1)
         s21.counter += 1
+        print(s21.get_count_info(datetime.now()))
     if i % 3 == 0:
         time.sleep(2)
         s31.counter += 1
-print(s11.get_count_info())
-print(s21.get_count_info())
-print(s31.get_count_info())
+        print(s31.get_count_info(datetime.now()))
+
+
 
 
