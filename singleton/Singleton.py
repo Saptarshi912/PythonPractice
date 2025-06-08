a4c8511206f2b05b9db16adcd9662b50a7805c88
Saptarshi <growthpersonal874@gmail.com>
2025-06-08 21:39:23 +0530

    added date time

--- singleton/Singleton.py
+++ singleton/Singleton.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class SingletonMeta(type):
     _instances = {}
     def __call__(cls,*args,**kwargs):
@@ -12,8 +13,13 @@ class Singleton(metaclass=SingletonMeta):
     def __init__(self,value):
         self.value = value
         self.counter = 0
-    def get_value(self):
-        return self.value
+        self.start_time = datetime.now()
+    def get_count_info(self):
+        return f"Counter: {self.counter}, " \
+               f"Start Time: {self.start_time.strftime('%Y-%m-%d %H:%M:%S')}," \
+               f"End Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+
+
 
 
 
